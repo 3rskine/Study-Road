12b28636077f8be9ed32d3494aac86a85babd87b
3rskine <kinelin72123@gmail.com>
2025-08-24 20:27:23 +0800

    0824 10222 decode

---
+++ uva/08-13/10222Decode the Mad man/10222sov.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+int main() {
+    string keyboard = "`1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+    string word;
+    while(getline (cin,word)){
+        for (char ch : word){
+        
+            if( ch == ' ') {
+                cout <<' ';
+        }
+        else{
+            char lowerCh = tolower(ch);
+            size_t gpstheCh = keyboard.find(lowerCh);
+            if (gpstheCh !=string::npos && gpstheCh >=2 )
+            {
+                cout << keyboard[gpstheCh - 2];
+            }
+            
+
+        } 
+        
+        }
+        cout<< endl;
+    }
+    return 0;
+}
